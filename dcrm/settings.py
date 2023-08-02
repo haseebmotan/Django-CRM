d29a74157af515cb66d9cafc97d3db01eff11602
Haseeb Motan <haseebmotan07@gmail.com>
2023-08-02 17:18:59 +0300

    Added sqlite3 database.

--- dcrm/settings.py
+++ dcrm/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'dcrm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'dcrm',
-        'USER': 'haseeb',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': '3306'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -110,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Istanbul'
 
 USE_I18N = True
 
