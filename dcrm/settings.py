36c4b458c15368c731dfad8a042ab5b16c9d9615
Haseeb Motan <haseebmotan07@gmail.com>
2023-08-03 15:56:44 +0300

    Changed settings.py for production

--- dcrm/settings.py
+++ dcrm/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-eefm_n&ien=cyh#ja1w3vgb*s=n-5#=47_)_7cg72_=br%gb4m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
